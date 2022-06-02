48e2fbfb849a07bbb29dbca00f35d4392d6899b4
sourabhsurana1008 <sourabhsurana1008@gmail.com>
2022-06-02 10:45:34 +0530

    Fix sheet design

--- src/controllers/salary.controller.ts
+++ src/controllers/salary.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import * as Helper from '../helpers/helper';
+import * as fs from 'fs';
 
 export class salaryController {
     
@@ -19,19 +20,27 @@ export class salaryController {
 
         const listOfMonth = getNext12Month(inputDate);
         const bonusDate = Helper.addMonths(new Date(req.params.date), 1);
+
         let formatedBonusDate = bonusDate.toISOString().split('T')[0];
         const listOfbonusMonth = getNext12Month(formatedBonusDate);
+        const salaryRecord = getSalaryDay(listOfMonth);
+        const bonusRecord = getBonusDay(listOfbonusMonth);
+        const fileName = `salary record ${inputDate}`;
 
+        await Helper.createXlsx([salaryRecord, bonusRecord],fileName ).then((response: any) => {
 
-        await Helper.createXlsx([getSalaryDay(listOfMonth), getBonusDay(listOfbonusMonth)]).then((response: any) => {
-
-            res.download(response.filename, 'report.xlsx', function (err) {
+            res.download(response.filename, `${fileName}.xlsx`, function (err) {
                 if (err) {
                     console.log(err);
                 }
+                fs.unlinkSync(response.filename); 
             })
         })
     }
+}
+
+function formatDate(date:Date) {
+    new Date(date).toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 }
 
 function getBonusDay(listOfMonth: any) {
@@ -41,18 +50,18 @@ function getBonusDay(listOfMonth: any) {
         var date = new Date(Date.UTC(element.year, element.month, 15));
         const d = new Date(new Date(date));
         let day = d.getDay();
-
+       
         if (day !== 0 && day !== 6) {
-            // console.log(d);
-            bonusDate.push({ date: d, day: Helper.getDayName(day), reason: "No change" })
+            bonusDate.push({ date: d.toString().slice(0, 15), day: Helper.getDayName(day), reason: "No change" })
         } else if (day === 0) {
             d.setDate(d.getDate() + 3);
-            bonusDate.push({ date: d, day: Helper.getDayName(day), reason: "Weekend is there so shifted next wednesday" })
+            bonusDate.push({ date: d.toString().slice(0, 15), day: Helper.getDayName(day), reason: "Weekend is there so shifted next wednesday" })
         } else {
             d.setDate(d.getDate() + 4);
-            bonusDate.push({ date: d, day: Helper.getDayName(day), reason: "Weekend is there so shifted next wednesday" })
+            bonusDate.push({ date: d.toString().slice(0, 15), day: Helper.getDayName(day), reason: "Weekend is there so shifted next wednesday" })
         }
     });
+    console.log(bonusDate)
     return bonusDate;
 }
 
@@ -64,14 +73,14 @@ function getSalaryDay(listOfMonth: any) {
         var days = [];
         while (date.getUTCMonth() === val.month) {
             const d = new Date(new Date(date));
-            let day = d.getDay();
-            days.push({ date: new Date(date), day: day });
+            let daynumber = d.getDay();
+            days.push({ date:  date.toString().slice(0, 15), day: daynumber, dayName: Helper.getDayName(daynumber) });
             date.setUTCDate(date.getUTCDate() + 1);
         }
         let obj = days.reverse();
-        let result = obj.filter(fal => {
-            if (fal.day <= 5 && fal.day >= 1) {
-                return fal
+        let result = obj.filter(val => {
+            if (val.day <= 5 && val.day >= 1) {
+                return val;
             }
         })
         response.push(result[0])
--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -1,10 +1,10 @@
-import ExcelJS from 'exceljs';
+import ExcelJS, { Worksheet } from 'exceljs';
 
-const createXlsx = async (data: any) => {
+const createXlsx = async (data: any, fileName:string) => {
 
     const { dirname } = require('path');
     const appDir = dirname(require.main.filename);
-    let filename = `${appDir}/report.xlsx`;
+    let filename = `${appDir}/${fileName}.xlsx`;
     const workbookWriter = new ExcelJS.stream.xlsx.WorkbookWriter({
         filename,
         useStyles: true
@@ -13,31 +13,30 @@ const createXlsx = async (data: any) => {
     const result = new Promise((resolve, reject) => {
         try {
             const salarySheet = workbookWriter.addWorksheet('salary Report')
-            salarySheet.columns = [{ header: 'date', key: 'date' }, { header: 'day', key: 'day' }];
-
-            if (data[0]) {
-                data[0].forEach((record: any) => {
-                    salarySheet.addRow(record);
-                })
-            }
+            salarySheet.columns = [{ header: 'date', key: 'date', width:30  }, 
+            { header: 'Actual Day on Salary Day', key: 'dayName',width:30  }];
+            data[0].forEach((record: any) => {
+                let row =   salarySheet.addRow(record);
+            })
+            styleSheet(salarySheet);
             salarySheet.commit()
             const bonusSheet = workbookWriter.addWorksheet('Bonus Report');
             bonusSheet.columns = [
-                { header: 'date', key: 'date' },
-                { header: 'Actual Day on 15th ', key: 'day' },
-                { header: 'reason', key: 'reason' }];
-
+                { header: 'date', key: 'date',width:30 },
+                { header: 'Actual Day on 15th ', key: 'day',width:30 },
+                { header: 'reason', key: 'reason',width:60 }];
             data[1].forEach((record: any) => {
-                bonusSheet.addRow(record);
+               let row =  bonusSheet.addRow(record);
             })
-
+            styleSheet(bonusSheet);
             bonusSheet.commit()
+
             workbookWriter.commit().then(data => {
                 resolve({ filename: filename });
             }).catch((err) => console.error(err))
-        } catch (error) {
-            reject(error)
-        }
+            } catch (error) {
+                reject(error)
+            }
 
     });
     return result;
@@ -61,9 +60,53 @@ const validateDate = (dateStr: string) => {
     const regex = /^\d{4}-\d{2}-\d{2}$/;
 
     if (dateStr.match(regex) === null) {
+        return 'Please enter date with YYYY-MM-DD format.';
+    }
+
+    const date = new Date(dateStr);
+
+    const timestamp = date.getTime();
+
+    if (typeof timestamp !== 'number' || Number.isNaN(timestamp)) {
         return 'Please enter valid date with YYYY-MM-DD format.';
     }
+
     return true;
+}
+
+const styleSheet = (sheet:Worksheet)=>{
+    return   sheet.eachRow({ includeEmpty: false }, function(row: any, rowNumber:number){
+        row.eachCell(function(cell:any, colNumber:number){
+         cell.font = {
+           name: 'Arial',
+           family: 2,
+           bold: true,
+           size: 10,
+         };
+         cell.alignment = {
+           vertical: 'middle', horizontal: 'center'
+         };
+        row.height = 20;
+        if (rowNumber >= 1) {
+           for (var i = 1; i < 4 ; i++) {
+             if (rowNumber == 1) {
+               row.getCell(i).fill = {
+                 type: 'pattern',
+                 pattern:'solid',
+                 fgColor:{argb:'C7C7C7'}
+               };
+             }
+             row.getCell(i).border = {
+             top: {style:'thin'},
+             left: {style:'thin'},
+             bottom: {style:'thin'},
+             right: {style:'thin'}
+           };
+         }
+        }
+       });
+    });
+
 }
 
 export { createXlsx, addMonths, getDayName, validateDate };
